e35428439668c9c1c6f995bdd0741843eeaac480
Nearchos Paspallis <NPaspallis@uclan.ac.uk>
2026-04-30 16:13:50 +0300

    various fixes

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "986885356" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "305045889" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
